e9bb59ddcbd6ad1dd38f7e8124f803014dcd9fab
nitesh.ghimire <nitesh.ghimire@f1soft.com>
2023-11-21 20:02:09 +0545

    Added other models improved results

--- main.py
+++ main.py
@@ -10,9 +10,23 @@ from sklearn.preprocessing import OneHotEncoder
 
 app = Flask(__name__)
 
-sub_mean = 22982412.06030151
+
+views_mean = 11249775535.65747
+views_std = 14808945108.497347
+views_max = 228000000000.0
+views_min = 0.0
 upload_mean = 9187.125628140704
-# upload_std = 34151.352253722594
+upload_std = 36352.7837345139
+upload_max = 301308
+upload_min = 0
+avg_mon_mean = 334093.7098103448
+avg_mon_std = 619998.0598726937
+avg_mon_max = 7225450.0
+avg_mon_min = 0
+avg_year_mean = 4011713.708798851
+avg_year_std = 7445617.173565455
+avg_year_max = 7225450.0
+avg_year_min = 0.0
 
 channel_type = ['Music', 'Games', 'Entertainment', 'Education', 'People', 'Sports',
                 'Film', 'News', 'Comedy', 'Howto', 'Nonprofit', 'Tech', 'Other',
@@ -41,41 +55,63 @@ def calculate_onehot(data):
         onehot_dict[ct] = onehot_encoded[idx]
     return onehot_dict
 
-viewers_predictor = pickle.load(open('./models/rf_viewers_predictor', 'rb'))
-income_predictor = pickle.load(open('./models/rf_income_predictor', 'rb'))
+def scaling(data, min, max):
+    scaled_value  = (data - min)/(max - min)
+    return scaled_value
+
+# viewers_predictor = pickle.load(open('./models/rf_viewers_predictor', 'rb'))
+# income_predictor = pickle.load(open('./models/rf_income_predictor', 'rb'))
+subscribers_predictor = pickle.load(open('./models/model6', 'rb'))
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
-    viewers_result = ''
-    income_result = ''
+    subscribers_result = ''
+    # income_result = ''
     if request.method == 'POST':
-        numSub = request.form['numSub']
+        numView = request.form['numView']
         try:
-            numSub = int(numSub)
+            numView = int(numView)
         except:
-            numSub = sub_mean
+            numView = sub_view
+        scaled_numView = scaling(numView, views_min, views_max)
         
         numUpload = request.form['numUpload']
         try:
             numUpload = int(numUpload)
         except:
             numUpload = upload_mean
-        channelType = request.form['channelType']
-        country = request.form['country']
-        country_onehot_dict = calculate_onehot(countries)
-        country_onehot = list(country_onehot_dict[country])
-        channel_onehot_dict = calculate_onehot(channel_type)
-        channel_onehot = list(channel_onehot_dict[channelType])
-        prediction_data = np.array([int(numSub), int(numUpload)]+country_onehot+channel_onehot)
+        scaled_numUpload = scaling(numUpload, upload_min, upload_max)
+
+        avg_monthly_income = request.form['monInc']
+        print(avg_monthly_income)
+        try:
+            avg_monthy_income = int(avg_monthy_income)
+        except:
+            avg_monthly_income = avg_mon_mean
+        scaled_avg_monthly_income = scaling(avg_monthly_income, avg_mon_min, avg_mon_max)
+
+        avg_yearly_income  = request.form['yearInc']
+        try:
+            avg_yearly_income = int(avg_yearly_income)
+        except:
+            avg_yearly_income = avg_year_mean
+        scaled_avg_yearly_income = scaling(avg_yearly_income, avg_year_min, avg_year_max)
+        # channelType = request.form['channelType']
+        # country = request.form['country']
+        # country_onehot_dict = calculate_onehot(countries)
+        # country_onehot = list(country_onehot_dict[country])
+        # channel_onehot_dict = calculate_onehot(channel_type)
+        # channel_onehot = list(channel_onehot_dict[channelType])
+        prediction_data = np.array([scaled_numView, scaled_numUpload, scaled_avg_monthly_income, scaled_avg_yearly_income])
         # print(prediction_data)
-        predicted_viewers = viewers_predictor.predict([prediction_data])[0]
-        viewers_result = f"The predicted viewers for this youtube channel is {predicted_viewers}"
-        predicted_income = income_predictor.predict([prediction_data])[0]
-        income_result = f"The predicted monthly income for this youtube channel is {predicted_income}"
+        predicted_subscribers = subscribers_predictor.predict([prediction_data])[0]
+        subscribers_result = f"The predicted subscribers for this youtube channel is {predicted_subscribers}"
+        # predicted_income = income_predictor.predict([prediction_data])[0]
+        # income_result = f"The predicted monthly income for this youtube channel is {predicted_income}"
         # print(predicted_viewers)
         # print(precicted_income)
 
-    return render_template('predict.html', channel_type = channel_type, countries = countries, viewers_result=viewers_result, income_result=income_result)
+    return render_template('predict.html', channel_type = channel_type, countries = countries, subscribers_result=subscribers_result)
 
 if __name__ == '__main__':
     app.run(debug=True)
